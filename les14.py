9da97433f76109605b19a31ce0d6bcd0678bdc26
Daniil Shapin <daniil.shapin327@gmail.com>
2025-01-29 20:42:13 +0300

    grad

---
+++ les14.py
@@ -0,0 +1,13 @@
+import torch
+
+#Специальное свойство обозначает что будут вычисляться производные по этим точкам
+x = torch.tensor([2.], requires_grad=True)
+y = torch.tensor([-4.], requires_grad=True)
+
+f = (x+y)**2 +2*x*y
+#вычисляет числовое значение по точке
+f.backward()
+
+print(f)
+print(x.data,x.grad)
+print(y.data,y.grad)
